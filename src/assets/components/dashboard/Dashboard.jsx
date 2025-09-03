32c8388474f44c40480c8a9c9acefb0b4b4050e5
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2025-09-03 12:17:31 +0800

    turning css to tailwind

--- src/assets/components/dashboard/Dashboard.jsx
+++ src/assets/components/dashboard/Dashboard.jsx
@@ -5,8 +5,8 @@ import DashboardContent from "./content/DashboardContent";
 
 function Dashboard() {
   return (
-    <div className="dashboard-container">
-      
+    //<div className="dashboard-container">
+    <div className="grid grid-cols-[1fr_auto] bg-[#F9F9F9] font-['Inter']">
       <SideBar/>
 
       <DashboardContent/>
