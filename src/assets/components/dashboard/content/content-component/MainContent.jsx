0f8be2765804e2824a15350420be691070114d3e
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2025-09-03 12:22:09 +0800

    reclassed MainContent using tailwind

--- src/assets/components/dashboard/content/content-component/MainContent.jsx
+++ src/assets/components/dashboard/content/content-component/MainContent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MainContent = ({children}) => {
   return (
-    <div className="main-content">
+    <div className="grid grid-cols-[minmax(320px,485px),1fr] grid-rows-[auto,auto,auto] gap-6">
         {children}
     </div>
   )
