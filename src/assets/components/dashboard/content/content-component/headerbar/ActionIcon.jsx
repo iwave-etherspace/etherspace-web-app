0fded9f792a8eda6b422787a9c41d8cf9664181a
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2025-09-04 00:04:54 +0800

    actionicon now has self contained css

--- src/assets/components/dashboard/content/content-component/headerbar/ActionIcon.jsx
+++ src/assets/components/dashboard/content/content-component/headerbar/ActionIcon.jsx
@@ -5,7 +5,7 @@ const ActionIcon = ({alt,src}) => {
     <img
         src={src}
         alt={alt}
-        className="action-icon"
+        className="w-[33px] h-[33px] cursor-pointer"
     />
   )
 }
