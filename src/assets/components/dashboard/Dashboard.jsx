f0b2246bc46a9c9ee8b2922487f2756bfe34dd65
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2025-09-06 02:19:00 +0800

    Fixed unaligned Mini UserProfile

--- src/assets/components/dashboard/Dashboard.jsx
+++ src/assets/components/dashboard/Dashboard.jsx
@@ -9,14 +9,14 @@ function Dashboard() {
   const activeContent = activeContentState[0];
   console.log("activeContent: ",activeContent);
   return (
-    <div className="h-dvh border-[1rem] border-amber-500 grid md:grid-cols-[10rem_7fr] grid-cols-1 grid-rows-[1fr_auto] gap-4">
+    <div className="h-dvh border-[1rem] border-amber-500 grid md:grid-cols-[10rem_7fr] grid-rows-[1fr_auto] gap-4">
       <div className="col-start-1 max-md:hidden">
         <SideBar activeContentState={activeContentState}/>
       </div>
-      <div>
+      
         <DashboardContent activeContent={activeContent} firstName={"Sharmaine"} lastName={"Kho"} userContact={"+(63) 913 142 5241"} profileImage={"src/assets/imgs/default-avatar-female.png"}/>
-      </div>
-      <div className="row-start-2 col-span-2 ">
+      
+      <div className="row-start-2 md:col-span-2 ">
         <div className="text-[#091237] text-[0.5rem] font-normal leading-5 self-end">powered by: etherspace</div>
       </div>
     </div>
