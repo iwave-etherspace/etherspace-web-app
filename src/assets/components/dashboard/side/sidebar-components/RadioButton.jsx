925cd6d436cde9507e5809510d9fcff944ecaac2
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2025-09-05 13:42:40 +0800

    Fixed NavSection alignment

--- src/assets/components/dashboard/side/sidebar-components/RadioButton.jsx
+++ src/assets/components/dashboard/side/sidebar-components/RadioButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const RadioButton = ({group,value,id,children}) => {
-    const actualId = group+id
+const RadioButton = ({group,value,children}) => {
+    const actualId = group+value
   return (
     <div className ="w-full">
         <input type="radio" name={group} value={value} id={actualId} class="hidden peer"/>
@@ -17,6 +17,7 @@ const RadioButton = ({group,value,id,children}) => {
               hover:bg-[#c96440]/10
               hover:text-black
               hover:fill-[#1D154A]
+              hover:stroke-[#1D154A]
 
                 transition-colors duration-200
                 
@@ -26,14 +27,16 @@ const RadioButton = ({group,value,id,children}) => {
                 peer-checked:hover:opacity-100
               peer-checked:hover:text-white
               peer-checked:hover:fill-white
+              peer-checked:hover:stroke-white
+
 
               peer-checked:fill-white
+              peer-checked:stroke-white
               fill-[#1D154A]
+              stroke-[#1D154A]
                 ">
-          <div  className="place-content-center ml-5 mr-2">
-                {children}
-          </div>
-          <span>{value}</span>
+            {children ? <div  className="place-content-center ml-5 mr-2">{children}</div> : null}
+            {children ? <span>{value}</span> : <span className="ml-2">{value}</span>}
 
         </label>
 
