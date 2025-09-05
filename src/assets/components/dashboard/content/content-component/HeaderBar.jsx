7d87dec6f10a70e7bfeb04cbe751cd75442f3c7c
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2025-09-06 00:17:02 +0800

    added burger icon

--- src/assets/components/dashboard/content/content-component/HeaderBar.jsx
+++ src/assets/components/dashboard/content/content-component/HeaderBar.jsx
@@ -5,10 +5,15 @@ import UserProfileIcon from './headerbar/UserProfileIcon'
 const HeaderBar = ({userName,userContact,src}) => {
   return (
     <header className="flex justify-between items-center gap-4">
-        <div className="flex items-center bg-white rounded-lg flex-1 max-w-full h-full">
-        <span className="ml-5 text-black text-base font-normal leading-5">ETHERSPACE - LOTTOMATIK</span>
+        <div>
+          <ActionIcon alt="Sidebar Collapse Button" src="src/assets/imgs/nav/burger.png"/>
+          <img 
+            alt=""
+            src=""></img>
         </div>
-
+        <div className="flex items-center bg-white rounded-lg flex-1 max-w-full h-full">
+          <span className="ml-5 text-black text-base font-normal leading-5">ETHERSPACE - LOTTOMATIK</span>
+        </div>
         <div className="flex items-center gap-4">
           <ActionIcon alt="Mail" src="src/assets/imgs/mail.png"/>
           <ActionIcon alt="Notifications" src="src/assets/imgs/bell.png"/>
