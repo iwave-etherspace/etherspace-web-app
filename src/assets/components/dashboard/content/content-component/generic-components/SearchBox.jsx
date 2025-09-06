b2d48629e2688de55f61deb440dff7e2ff85d31c
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2025-09-06 15:15:45 +0800

    removed justify-end class in SearchBox component

--- src/assets/components/dashboard/content/content-component/generic-components/SearchBox.jsx
+++ src/assets/components/dashboard/content/content-component/generic-components/SearchBox.jsx
@@ -2,27 +2,27 @@ import React from 'react'
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 const SearchBox = ({query,setQuery}) => {
   return (
-    <div className="grid justify-end">
-          <div className="relative flex place-items-center">
+    
+        <div className="relative flex place-items-center">
             <div className="absolute pl-5">
-              <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
+                <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
             </div>
             <input
-              className="
-              w-[15rem]
-              font-sans
-              pl-15 py-2
-              rounded-xl bg-white border border-gray-200
-              focus:outline-none focus:ring-2 focus:ring-blue-200
-              "
-              type="search"
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              placeholder="Search something"
-              aria-label="Search table"
+                className="
+                w-[15rem]
+                font-sans
+                pl-15 py-2
+                rounded-xl bg-white border border-gray-200
+                focus:outline-none focus:ring-2 focus:ring-blue-200
+                "
+                type="search"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Search something"
+                aria-label="Search table"
             />
-          </div>
         </div>
+
   )
 }
 
--- src/assets/components/dashboard/content/pages/history/BalanceHistory.jsx
+++ src/assets/components/dashboard/content/pages/history/BalanceHistory.jsx
@@ -59,29 +59,9 @@ export default function BalanceHistory() {
         <h2 className="text-lg font-semibold text-gray-800 font-sans">
           Balance History
         </h2>
-        <SearchBox query={query} setQuery={setQuery}/>
-        {/* <div className="grid justify-end">
-          <div className="relative flex place-items-center">
-            <div className="absolute pl-5">
-              <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
-            </div>
-            <input
-              className="
-              w-[15rem]
-              font-sans
-              pl-15 py-2
-              rounded-xl bg-white border border-gray-200
-              focus:outline-none focus:ring-2 focus:ring-blue-200
-              "
-              type="search"
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              placeholder="Search something"
-              aria-label="Search table"
-            />
-          </div>
-        </div> */}
-        
+        <div className="grid justify-end">
+          <SearchBox query={query} setQuery={setQuery}/>
+        </div>
       </div>
 
       {/* Table for medium+ screens */}
