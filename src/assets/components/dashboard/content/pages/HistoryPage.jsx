49074e53945f49de4039b990789fbcbc280fbbf7
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2025-09-07 10:26:54 +0800

    refactored import directory

--- src/assets/components/dashboard/content/pages/HistoryPage.jsx
+++ src/assets/components/dashboard/content/pages/HistoryPage.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import MainContent from '../content-component/MainContent'
 import ContentCell from '../content-component/maincontent/ContentCell'
-import BalanceHistory from './history/BalanceHistoryDepr'
-
-import PaymentHistory from './history/PaymentHistoryDepr'
+import BalanceHistory from './history/BalanceHistory'
+import PaymentHistory from './history/PaymentHistory'
 
 
 
 const HistoryPage = () => {
   return (
     <MainContent>
-        {/* <div className="h-full grid grid-rows-[repeat(2,auto)] content-start gap-4"> */}
         <div className="h-full grid grid-flow-row content-start gap-4">
             <ContentCell cStart={1} cSpan={1} rSpan={1}>
                 <BalanceHistory />
