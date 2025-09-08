280f19864323f7972ddeae518b97983a4c91703d
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2025-09-08 09:50:01 +0800

    removed deprecated History components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./assets/styles/dashboard.css";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterPage from "./assets/components/RegisterPage";
 import OtpPage from "./assets/components/OtpPage";
 import AccountSetupPage from "./assets/components/AccountSetupPage";
 import Dashboard from "./assets/components/dashboard/Dashboard";
-import "./assets/styles/dashboard.css";
-import History from "./assets/components/history/History";
 import RegisterWithPasswordPage from "./assets/components/RegisterWithPasswordPage";
 
 function App() {
@@ -19,7 +19,6 @@ function App() {
         element={<RegisterWithPasswordPage />}
       ></Route>
       <Route path="/accountSetupPage" element={<AccountSetupPage />}></Route>
-      <Route path="/history" element={<History />}></Route>
       <Route path="/dashboard" element={<Dashboard />}></Route>
     </Routes>
   );
